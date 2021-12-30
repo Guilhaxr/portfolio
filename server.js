e66dd834483230c58b5ac6968d56fabc69c38e27
Guilherme R <guilhaxr123@gmail.com>
2021-12-30 16:54:02 +0000

    starting server

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+mongoose
+   .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efgkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+   )
+   .then((result) => console.log('Connected to MONGO ATLAS'))
+   .catch((err) => console.log(err));
+
+
+
+   const port = process.env.PORT || 5000;
+
+   app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+ });

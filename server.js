06361827c25d531ebb649a1804a95d255f048bad
Guilherme R <guilhaxr123@gmail.com>
2022-01-02 18:49:26 +0000

    concurrently server and client

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -8,12 +8,15 @@ const app = express();
 
 mongoose
    .connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efgkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efgkj.mongodb.net/${process.env.DB_PROJECT}?retryWrites=true&w=majority`
    )
    .then((result) => console.log('Connected to MONGO ATLAS'))
    .catch((err) => console.log(err));
 
 
+   app.use((req, res) =>
+   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+);
 
    const port = process.env.PORT || 5000;
 

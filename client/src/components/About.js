fe913f172bc20716994f45270b3e780ffe47eada
Guilherme R <guilhaxr123@gmail.com>
2022-01-14 21:06:19 +0000

    skils bars done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -49,7 +49,7 @@ const About = ({HandlerButton}) => {
         </div>
         <div className="windowInformation" style={windowAboutStyle}>
             <div className="personalInformation">
-                <img src="https://cvws.icloud-content.com/B/Aaiz7pQsLVy7hDeh6qhR_9YraUFYAembJ2e4THl3pXivdUpFWsH7Av7_/fotoSemFundo.png?o=Ampqq4JXSbBjFkWTyLuw45khKwPSgkLcU5Y8Ukq3nwnq&v=1&x=3&a=CAogqbohXVvfJCc-cLDPR8QkJ05PdYjeZeCioqpcaKNYUncSbxCP1_zC5S8Yr86zw-UvIgEAUgQraUFYWgT7Av7_aic5ZkjwpqCcJ2HA_TTvujJm9KeuiBW79BjOXWn3jWg0T3Q1S5Hkan5yJ6Du4L7KssSw_AEV6uTozxWBLKsIsRc3TvljelcXlFZ3Lb4jz1FEgg&e=1642161039&fl=&r=ee223b31-9a2a-4449-8f75-f4ff56a9f522-1&k=jQ3Pw2Q66TeHfg3YXupW2w&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=52&s=PGJxFNiWPprSr4K6KOQqUT5x4M8&cd=i" alt="myPhoto" />
+                <img src="https://cvws.icloud-content.com/B/Aaiz7pQsLVy7hDeh6qhR_9YraUFYAembJ2e4THl3pXivdUpFWsH7Av7_/fotoSemFundo.png?o=AqC8WV-pYz8Y9E7b--k5euAUpccvdv2E_owShiOrtjwy&v=1&x=3&a=CAogw8iUJe2JpvBxVVdjEhxVmATaDJYKnDCE7EnDtLjgitcSbxDNqJTO5S8Y7Z_LzuUvIgEAUgQraUFYWgT7Av7_aifCCFYAtNC6hskyglQ-bFsB0F0LxJYVpuVlrcT9A_QdCkBNq1NUvbRyJ96QblyoSBf6WoTQcvHqW4gaIKBWLjtXjtIcBz1azUKZffAxp_CwLQ&e=1642184495&fl=&r=6d3d66a8-1a1f-4582-88f5-8000ff7c23a3-1&k=jQ3Pw2Q66TeHfg3YXupW2w&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=52&s=NkBm8Y9I8m7RguQtERwvwQGdbVs&cd=i" alt="myPhoto" />
                 <h4>Guilherme Rodrigues</h4>
                 <h6>Full Stack Web Developer</h6>
             </div>
@@ -62,10 +62,65 @@ const About = ({HandlerButton}) => {
                         It was an intensive and very challenging course because I had to learn very quickly some programming vocabulary, languages and technologies.
                         I also developed mini-games and applications which two of them were in "Hackathon" format. </p>
                 </div>
-                <div className="skills">
-                    skills
+                <div className="techSkills">
+                    <h1>Skills</h1>
+                    <div className="skillBox">
+                        <p>Javascript</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level" style={{width:"75%"}}></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>React.js</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level" style={{width:"60%"}}></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>HTML</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>CSS</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>Node.js</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>Express</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>MondoDB</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
+                    <div className="skillBox">
+                        <p>MYSQL</p>
+                        <p>65%</p>
+                            <div className="skill">
+                                <div className="skill-level"></div>
+                            </div>
+                    </div>
                 </div>
-                
             </div>
            
 
